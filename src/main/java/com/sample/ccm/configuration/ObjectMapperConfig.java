7fe7e7c4918e5869055c95e02aba7e95f13f8329
JonathanLach <jonathanlachapellee@gmail.com>
2021-11-15 11:56:42 +0100

    Missing @Bean

--- src/main/java/com/sample/ccm/configuration/ObjectMapperConfig.java
+++ src/main/java/com/sample/ccm/configuration/ObjectMapperConfig.java
@@ -3,6 +3,7 @@ package com.sample.ccm.configuration;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class ObjectMapperConfig {
